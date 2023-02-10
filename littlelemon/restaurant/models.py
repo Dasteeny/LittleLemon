029ac484830e079766cb4381586f6da13c5e3d01
Dasteeny <dastan.zhabagin@gmail.com>
2023-02-11 01:41:20 +0600

    Add authentication

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -6,8 +6,17 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
 
+    def get_item(self):
+        return f"{self.title} : {self.price}"
+
+    def __str__(self) -> str:
+        return str(self.title)
+
 
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
     booking_date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return str(self.name)
