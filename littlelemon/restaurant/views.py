52d00f456c730bf2bc8fb8cc4476fb3d2e323a34
Dasteeny <dastan.zhabagin@gmail.com>
2023-02-10 17:21:36 +0600

    Add API endpoints

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from restaurant.models import Booking, Menu
+from restaurant.serializers import BookingSerializer, MenuSerializer
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
 
 
 def index(request):
